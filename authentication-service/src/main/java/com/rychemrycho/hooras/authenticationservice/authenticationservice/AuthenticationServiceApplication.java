5955b926f3e1804954cdc09012b497184a6f956d
Rych Emrycho <rychemrycho@gmail.com>
2020-03-26 08:59:05 +0700

    Initial commit

---
+++ authentication-service/src/main/java/com/rychemrycho/hooras/authenticationservice/authenticationservice/AuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rychemrycho.hooras.authenticationservice.authenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServiceApplication.class, args);
+    }
+
+}
---
+++ authentication-service/src/test/java/com/rychemrycho/hooras/authenticationservice/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rychemrycho.hooras.authenticationservice.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
